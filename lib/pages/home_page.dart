3a8e016b915fd59e6df7a21b6ce1db71c6a2fce0
pedro420b <badrxxxpedro@gmail.com>
2024-09-13 12:37:00 +0100

    v0012

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,9 +10,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home"),
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget>[
+          Container( //Header
+            height: 150,
+            color: const Color.fromARGB(255, 155, 136, 91),
+            child: const Center(child: Text('Header')),
+          ),
+          Container( //Body
+            height: 550,
+            color: Colors.amber[500],
+            child: const Center(child: Text('Body')),
+          ),
+          Container(//Footer
+            height: 250,
+            color: const Color.fromARGB(255, 187, 183, 172),
+            child: const Center(child: Text('Footer')),
+          ),
+        ],
       ),
     );
   }
