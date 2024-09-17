52784b05854a09cb0392816684271a60a1b34d20
pedro420b <badrxxxpedro@gmail.com>
2024-09-17 18:03:35 +0100

    v00121

--- lib/Blocs/registration_bloc.dart
+++ lib/Blocs/registration_bloc.dart
@@ -7,33 +7,13 @@ class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
   final FirebaseAuth _firebaseAuth;
 
   RegistrationBloc(this._firebaseAuth) : super(const RegistrationState()) {
-    on<EmailChanged>((event, emit) {
-      emit(state.copyWith(email: event.email));
-    });
-
-    on<PasswordChanged>((event, emit) {
-      emit(state.copyWith(password: event.password));
-    });
+  
 
     on<RegisterSubmitted>((event, emit) async {
       emit(state.copyWith(isSubmitting: true, errorMessage: null));
-      try {
-        UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
-          email: event.email,
-          password: event.password,
-        );
-        emit(state.copyWith(isSuccess: true, isSubmitting: false));
-      } on FirebaseAuthException catch (e) {
-        String errorMessage;
-        if (e.code == 'weak-password') {
-          errorMessage = 'The password provided is too weak.';
-        } else if (e.code == 'email-already-in-use') {
-          errorMessage = 'The account already exists for that email.';
-        } else {
-          errorMessage = e.message ?? 'Registration failed.';
-        }
-        emit(state.copyWith(isSubmitting: false, errorMessage: errorMessage));
-      }
-    });
+     
+      });
+       
+    }
   }
-}
+
--- lib/Events/registration_event.dart
+++ lib/Events/registration_event.dart
@@ -7,24 +7,6 @@ abstract class RegistrationEvent extends Equatable {
   List<Object?> get props => [];
 }
 
-class EmailChanged extends RegistrationEvent {
-  final String email;
-
-  const EmailChanged(this.email);
-
-  @override
-  List<Object?> get props => [email];
-}
-
-class PasswordChanged extends RegistrationEvent {
-  final String password;
-
-  const PasswordChanged(this.password);
-
-  @override
-  List<Object?> get props => [password];
-}
-
 class RegisterSubmitted extends RegistrationEvent {
   final String email;
   final String password;
