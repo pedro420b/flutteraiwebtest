d48cdc850dbe17f73fe4323872f27973dec8e9bb
pedro420b <badrxxxpedro@gmail.com>
2024-09-22 17:45:09 +0100

    d0004

--- lib/pruefungsimu/beispiel.dart
+++ lib/pruefungsimu/beispiel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutteraiwebtest/Models/teiltext.dart';
 class Beispiel extends StatefulWidget {
   const Beispiel({super.key});
 
@@ -14,6 +16,8 @@ class _BeispielState extends State<Beispiel> {
 
   FlutterTts tts1 = FlutterTts();
 
+  final CountDownController _controller=CountDownController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,30 +27,44 @@ class _BeispielState extends State<Beispiel> {
       ),
       body: Column(
         children: <Widget> [
-          Row(
-            children: [
-              Container(
-                height: 100,
-                width: 90,
-                decoration: const BoxDecoration(
-                  color: Colors.deepOrange,
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              
+              children: [
+                const Text(
+                  "Teil 1",style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-              ),
-                Container(
-                height: 100,
-                width: 110,
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 41, 24, 19),
+                const SizedBox(
+                  width: 100,
+                )
+                ,
+                  const Text(
+                  "Vorgeschlagene Arbeitszeit: 10 Minuten",style: TextStyle(fontWeight: FontWeight.normal),
                 ),
-              ),
-            ],
-          ),
-            Container(
-            height: 100,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 178, 178, 178),
+                const SizedBox(
+                  width: 20,
+                ),
+                CircularCountDownTimer(
+                  width: 40,
+                   height: 30,
+                   controller: _controller,
+                    duration: 100,
+                     fillColor: const Color.fromARGB(255, 115, 225, 121),
+                      ringColor: const Color.fromARGB(255, 233, 66, 63))
+              ],
             ),
+          ),
+          const SizedBox(
+            height: 20,
           )
+          ,
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Center(
+                child: Center(child:  Text(teil1tetxtbeispiel)),
+              ),
+            )
 
         ],
       ),
