7a217048c72e294b8983bc1547d08ee2d7e369f7
pedro420b <badrxxxpedro@gmail.com>
2024-09-25 18:20:36 +0100

    nur noch ein paar Sachen mussen gemacht werden

--- lib/pruefungsimu/beispiel.dart
+++ lib/pruefungsimu/beispiel.dart
@@ -314,6 +314,196 @@ class _BeispielState extends State<Beispiel> {
               ],
             ),
           ),
+          ////////////////////////////////////////////////////////////
+          //////////////////////Teil3Anfang//////////////////////////////////////
+          ///
+      Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                const Text(
+                  "Teil 3",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  width: 100,
+                ),
+                const Text(
+                  "Vorgeschlagene Arbeitszeit: 20 Minuten",
+                  style: TextStyle(fontWeight: FontWeight.normal),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                CircularCountDownTimer(
+                    width: 70,
+                    height: 70,
+                    autoStart: false,
+                    isTimerTextShown: true,
+                    isReverse: true,
+                    controller: _controllerteil3,
+                    duration: 1200,
+                    fillColor: const Color.fromARGB(255, 3, 4, 3),
+                    ringColor: const Color.fromARGB(255, 233, 66, 63)),
+                const SizedBox(
+                  width: 20,
+                ),
+                FloatingActionButton.extended(
+                  onPressed: _controllerteil3.start,
+                  label: const Text("Anfangen"),
+                  icon: const Icon(Icons.timer),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          //////////////////////////////////////////////////////////////////////
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: RichText(
+                text: TextSpan(
+                  style: const TextStyle(color: Colors.black, fontSize: 16),
+                  children: [
+                    TextSpan(
+                      text: Teil1Gabbeispiel0,
+                    ),
+                    // Use a WidgetSpan to embed the dropdown in the text
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Teil1DropDown(
+                        items: itemsgabbeispiel0,
+                        hintText: 'Beispiel 0',
+                        onChanged: (value) {
+                          print('Selected: $value');
+                        },
+                      ),
+                    ),
+                    TextSpan(
+                      text: Teil1Gab1,
+                    ),
+                    // Use a WidgetSpan to embed the dropdown in the text
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Teil1DropDown(
+                        items: itemsgab1,
+                        hintText: '1',
+                        onChanged: (value) {
+                          print('Selected: $value');
+                        },
+                      ),
+                    ),
+                    TextSpan(
+                      text: Teil1Gab2,
+                    ),
+                    // Use a WidgetSpan to embed the dropdown in the text
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Teil1DropDown(
+                        items: itemsgab2,
+                        hintText: '2',
+                        onChanged: (value) {
+                          print('Selected: $value');
+                        },
+                      ),
+                    ),
+                    TextSpan(
+                      text: Teil1Gab3,
+                    ),
+                    // Another Dropdown
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Teil1DropDown(
+                        items: itemsgab3,
+                        hintText: '3',
+                        onChanged: (value) {
+                          print('Selected: $value');
+                        },
+                      ),
+                    ),
+                    TextSpan(
+                      text: Teil1Gab4,
+                    ),
+                    // Use a WidgetSpan to embed the dropdown in the text
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Teil1DropDown(
+                        items: itemsgab4,
+                        hintText: '4',
+                        onChanged: (value) {
+                          print('Selected: $value');
+                        },
+                      ),
+                    ),
+                    TextSpan(
+                      text: Teil1Gab5,
+                    ),
+                    // Use a WidgetSpan to embed the dropdown in the text
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Teil1DropDown(
+                        items: itemsgab5,
+                        hintText: '5',
+                        onChanged: (value) {
+                          print('Selected: $value');
+                        },
+                      ),
+                    ),
+                    TextSpan(
+                      text: Teil1Gab6,
+                    ),
+                    // Another Dropdown
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Teil1DropDown(
+                        items: itemsgab6,
+                        hintText: '6',
+                        onChanged: (value) {
+                          print('Selected: $value');
+                        },
+                      ),
+                    ),
+                    TextSpan(
+                      text: Teil1Gab7,
+                    ),
+                    // Use a WidgetSpan to embed the dropdown in the text
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Teil1DropDown(
+                        items: itemsgab7,
+                        hintText: '7',
+                        onChanged: (value) {
+                          print('Selected: $value');
+                        },
+                      ),
+                    ),
+                    TextSpan(
+                      text: Teil1Gab8,
+                    ),
+                    // Use a WidgetSpan to embed the dropdown in the text
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Teil1DropDown(
+                        items: itemsgab8,
+                        hintText: '8',
+                        onChanged: (value) {
+                          print('Selected: $value');
+                        },
+                      ),
+                    ),
+                    TextSpan(
+                      text: Teil1Gab9,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+
+      //////////////////////////////////////////////////////////////////////////////////
           ///////////////////////////////////////////////////////////Teil4anfang////////
           Padding(
             padding: const EdgeInsets.all(16.0),
