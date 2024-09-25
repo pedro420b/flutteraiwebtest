efbd7baef77ea08f61028713e04f70fe043b77c2
pedro420b <badrxxxpedro@gmail.com>
2024-09-25 10:58:07 +0100

    teil1lesen fast zum ende , muss ich noch die anderen Teil2 und Teil3 machen

--- lib/Models/lesenteil1texts/lesenteil1text.dart
+++ lib/Models/lesenteil1texts/lesenteil1text.dart
@@ -1,11 +1,34 @@
-String Teil1Gab1="";
-String Teil1Gab2="";
-String Teil1Gab3="";
-String Teil1Gab4="";
-String Teil1Gab5="";
-String Teil1Gab6="";
-String Teil1Gab7="";
-String Teil1Gab8="";
-String Teil1Gab9="";
+String Teil1Gabbeispiel0="Unser Gehirn macht zwar nur ein Fünfzigstel unseres Körpergewichts aus, verbraucht";
+List<String> itemsgabbeispiel0=['typesetting', 'designing', 'developing'];
+
+String Teil1Gab1=" aber ein Fünftel unserer Energie. Kein Wunder, denn es ist immer im Dienst - und immer hungrig. Es gibt viele Lebensmittel, mit denen wir unsere grauen Zellen unterstützen können. Doch das Wichtigste ist ausreichend Flüssigkeit.";
+List<String> itemsgab1=['typesetting', 'designing', 'developing'];
+
+
+String Teil1Gab2="Nichts braucht unser Gehirn so sehr wie Wasser. Reicht die Flüssigkeit nicht aus, schrumpfen die Gehirnzellen, die Neuronen, und funktionieren nicht mehr richtig. Das Gehirn kann dann nicht mehr gut denken. Um es davor zu schützen, sollten wir wenigstens zwei Liter Wasser pro Tag trinken.";
+List<String> itemsgab2 =['book', 'tree', 'car', 'plane'];
+
+String Teil1Gab3="Grundsätzlich gilt: Ungesunde Nahrungsmittel, die Herzinfarkte oder Schlaganfälle begünstigen, fördern auch Durchblutungsstörungen im Gehirn, die lange unbemerkt bleiben können. Deshalb sollte man möglichst frühzeitig auf eine gefäßgesunde Ernährung achten. Auch wenn es sich in jedem Alter lohnen kann, seine Ernährung auf eine gesündere Ernährungsform wie die mediterrane Küche umzustellen, ist der Effekt für das Gehirn umso größer, je früher man beginnt. Wer mit 50 Jahren anfängt, sich gesünder zu ernähren, hat ein geringeres Risiko die Alzheimer-Krankheit zu bekommen, als wer sich erst mit 80 Jahren damit befasst";
+List<String> itemsgab3 =['book', 'tree', 'car', 'plane'];
+
+String Teil1Gab4="Flavonoide stecken in vielen Obst- und Gemüsesorten und sorgen für deren Färbung, etwa in Beeren, Äpfeln, Paprika und Zwiebeln. Auch in Kakao, grünem und schwarzem Tee sind diese sekundären Pflanzenstoffe enthalten. Flavonoide können die Blutgefäße elastisch halten und so das Risiko für einen Schlaganfall, Herzinfarkt und Arterienverkalkung insgesamt senken.";
+List<String> itemsgab4 =['book', 'tree', 'car', 'plane'];
+
+String Teil1Gab5="Flavonoidreiche Lebensmittel schützen die Gehirnfunktion. Insbesondere, wenn sie lebenslang gegessen werden. Eine Studie der Columbia University zeigte einen positiven Effekt auch bei Älteren. Untersucht wurde der Effekt von Flavonolen, einer Unterart der Flavonoide, auf die Gedächtnisleistung. Gerade ältere Menschen, die sich länger flavonol-arm ernährt hatten, profitierten von der täglichen Einnahme eines kakaohaltigen Flavonolextraktes. Nach einem Jahr normalisierte sich die Gedächtnisleistung. Auch andere Forschende sehen einen gesundheitlichen Effekt von Flavonoiden bei Menschen, die sich zuvor weniger gut ernährt haben.";
+List<String> itemsgab5 =['book', 'tree', 'car', 'plane'];
+
+String Teil1Gab6="Unser Gehirn braucht 120 bis 140 Gramm Glukose pro Tag, um ausreichend mit Energie versorgt zu werden. Bekommt es zu wenig, schaltet es auf Sparflamme und verliert an Leistungsfähigkeit. Um schnell wieder besser denken zu können, greifen viele Menschen in solchen Momenten zu Traubenzucker. Doch das ist nur eine Notlösung, denn für die Hirnfunktion ist ein stabiler Kohlenhydratspiegel wichtiger, während eine schwankende Energieversorgung die Funktion der grauen Zellen eher einschränkt.Gute Energielieferanten für das Gehirn sind Haferflocken, denn sie lassen den Blutzuckerspiegel langsam steigen. Hafer gilt zudem als Muntermacher, beugt Nervosität, Stress und Konzentrationsproblemen vor. Das Getreide ist ein hervorragender Nährstofflieferant, enthält neben reichlich Ballaststoffen unter anderem die Vitamine B1 und B6.";
+List<String> itemsgab6 =['book', 'tree', 'car', 'plane'];
+
+String Teil1Gab7="Nüsse sind reich an ungesättigten Fettsäuren und B-Vitaminen. Vor allem Walnüsse gelten als Brainfood, denn sie liefern neben Omega-3-Fettsäuren auch Eiweiß und die Vitamine B1, B2, B6 und E. Die B-Vitamine sind wichtig für die Signalübermittlung zwischen den einzelnen Nervenzellen und den Aufbau der sogenannten Myelinscheide, die als Isolierschicht dafür sorgt, dass Nervenimpulse schnell übertragen werden.";
+List<String> itemsgab7 =['book', 'tree', 'car', 'plane'];
+
+
+String Teil1Gab8="Für den internen Informationsaustausch, also beim Lernen und Erinnern, benötigt unser Gehirn auch reichlich Aminosäuren. Die bekommt es aus Eiweiß, zum Beispiel aus Eiern oder Quark, aber auch aus pflanzlichen Quellen: Kichererbsen liefern neben Eiweiß reichlich B-Vitamine und auch Sojabohnen (zum Beispiel Sojamilch, Tofu) sind nicht nur gute Eiweißquellen, sondern enthalten auch die Vitamine B1, B2 und B6, dazu Eisen, Magnesium und Zink. Edamame sind unreif geerntete Sojabohnen, die zudem reich an pflanzlichen Omega-3-Fettsäuren sind.";
+List<String> itemsgab8 =['book', 'tree', 'car', 'plane'];
+
+String Teil1Gab9="Fisch liefert reichlich Omega-3-Fettsäuren, genauer die Fettsäuren DHA und EPA. Sie wirken entzündungshemmend und halten die Wände unserer Zellen geschmeidig. Davon profitieren auch die grauen Zellen, denn für Verfallsprozesse im Gehirn sind oft Entzündungen mitverantwortlich. Nüsse, insbesondere Walnüsse, sind pflanzliche Lieferanten für Omega-3-Fettsäuren.";
+List<String> itemsgab9 =['book', 'tree', 'car', 'plane'];
+
 
 
--- lib/pruefungsimu/beispiel.dart
+++ lib/pruefungsimu/beispiel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutteraiwebtest/Models/lesenteil1texts/lesenteil1text.dart';
 import 'package:flutteraiwebtest/Models/teiltext.dart';
 import 'package:flutteraiwebtest/widgets/lesenwidget/teil1dropdownwidget.dart';
 
@@ -46,13 +47,16 @@ void stopSpeaking() async {
         appBar: AppBar(
           title: const Text("Sim Beispiel"),
         ),
-        body: Column(
+        body: ListView(
           children: <Widget>[
             ElevatedButton(onPressed:(){
               speakText(teil1tetxtbeispiel);
 
             } ,
-             child: const Text("Ausprechen"))
+             child: const Text("Ausprechen"),
+              
+             
+             )
             ,
             Padding(
               padding: const EdgeInsets.all(16.0),
@@ -80,7 +84,7 @@ void stopSpeaking() async {
                       isReverse: true,
                       controller: _controller,
                       duration: 600,
-                      fillColor: const Color.fromARGB(255, 115, 225, 121),
+                      fillColor: const Color.fromARGB(255, 3, 4, 3),
                       ringColor: const Color.fromARGB(255, 233, 66, 63)),
                       const SizedBox(
                         width: 20,
@@ -97,17 +101,6 @@ void stopSpeaking() async {
             const SizedBox(
               height: 20,
             ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Center(
-                child: Center(child: Text(teil1tetxtbeispiel)),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            )
-            ,
-            const Teil1DropDown(items: ["item1","item2"],hintText: "Beispiel 0",),
             //////////////////////////////////////////////////////////////////////
           Padding(
         padding: const EdgeInsets.all(16.0),
@@ -117,28 +110,126 @@ void stopSpeaking() async {
               style: const TextStyle(color: Colors.black, fontSize: 16),
               children: [
                 TextSpan(
-                  text: teil1tetxtbeispiel,
+                  text: Teil1Gabbeispiel0,
                 ),
                 // Use a WidgetSpan to embed the dropdown in the text
                 WidgetSpan(
                   alignment: PlaceholderAlignment.middle,
                   child: Teil1DropDown(
-                    items: ['typesetting', 'designing', 'developing'],
-                    hintText: 'Select',
+                    items: itemsgabbeispiel0,
+                    hintText: 'Beispiel 0',
                     onChanged: (value) {
                       print('Selected: $value');
                     },
                   ),
                 ),
                 TextSpan(
-                  text: teil1tetxtbeispiel,
+                  text: Teil1Gab1,
                 ),
                 // Use a WidgetSpan to embed the dropdown in the text
                 WidgetSpan(
                   alignment: PlaceholderAlignment.middle,
                   child: Teil1DropDown(
-                    items: ['typesetting', 'designing', 'developing'],
-                    hintText: 'Select',
+                    items: itemsgab1,
+                    hintText: '1',
+                    onChanged: (value) {
+                      print('Selected: $value');
+                    },
+                  ),
+                ),
+                TextSpan(
+                  text: Teil1Gab2,
+                ),
+                // Use a WidgetSpan to embed the dropdown in the text
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Teil1DropDown(
+                    items: itemsgab2,
+                    hintText: '2',
+                    onChanged: (value) {
+                      print('Selected: $value');
+                    },
+                  ),
+                )
+                ,
+                TextSpan(
+                  text: Teil1Gab3,
+                ),
+                // Another Dropdown
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Teil1DropDown(
+                    items:itemsgab3,
+                    hintText: '3',
+                    onChanged: (value) {
+                      print('Selected: $value');
+                    },
+                  ),
+                ),TextSpan(
+                  text: Teil1Gab4,
+                ),
+                // Use a WidgetSpan to embed the dropdown in the text
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Teil1DropDown(
+                    items: itemsgab4,
+                    hintText: '4',
+                    onChanged: (value) {
+                      print('Selected: $value');
+                    },
+                  ),
+                ),
+                TextSpan(
+                  text: Teil1Gab5,
+                ),
+                // Use a WidgetSpan to embed the dropdown in the text
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Teil1DropDown(
+                    items: itemsgab5,
+                    hintText: '5',
+                    onChanged: (value) {
+                      print('Selected: $value');
+                    },
+                  ),
+                )
+                ,
+                TextSpan(
+                  text: Teil1Gab6,
+                ),
+                // Another Dropdown
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Teil1DropDown(
+                    items:itemsgab6,
+                    hintText: '6',
+                    onChanged: (value) {
+                      print('Selected: $value');
+                    },
+                  ),
+                ),TextSpan(
+                  text: Teil1Gab7,
+                ),
+                // Use a WidgetSpan to embed the dropdown in the text
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Teil1DropDown(
+                    items: itemsgab7,
+                    hintText: '7',
+                    onChanged: (value) {
+                      print('Selected: $value');
+                    },
+                  ),
+                ),
+                TextSpan(
+                  text: Teil1Gab8,
+                ),
+                // Use a WidgetSpan to embed the dropdown in the text
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Teil1DropDown(
+                    items: itemsgab8,
+                    hintText: '8',
                     onChanged: (value) {
                       print('Selected: $value');
                     },
@@ -146,25 +237,7 @@ void stopSpeaking() async {
                 )
                 ,
                  TextSpan(
-                  text:teil1tetxtbeispiel,
-                ),
-                const TextSpan(
-                  text: ' Here is another gap: ',
-                ),
-                // Another Dropdown
-                WidgetSpan(
-                  alignment: PlaceholderAlignment.middle,
-                  child: Teil1DropDown(
-                    items: ['book', 'tree', 'car', 'plane'],
-                    hintText: 'Select',
-                    onChanged: (value) {
-                      print('Selected: $value');
-                    },
-                  ),
-                ),
-                const TextSpan(
-                  text:
-                      ' has been in use for over 500 years and is still used today.',
+                  text:Teil1Gab9,
                 ),
               ],
             ),
