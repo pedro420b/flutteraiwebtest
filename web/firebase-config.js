3549e611272e2326ce48223479fae80e9518eb04
badr e <163610155+pedro420b@users.noreply.github.com>
2024-10-02 00:37:20 +0100

    Update firebase-config.js

--- web/firebase-config.js
+++ web/firebase-config.js
@@ -7,13 +7,7 @@
    // Your web app's Firebase configuration
    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
    const firebaseConfig = {
-   apiKey: "AIzaSyCt8J3IbNvPkQOemJFttcDG1jZShVCmTtI",
-   authDomain: "[pruesimul.firebaseapp.com](http://pruesimul.firebaseapp.com/)",
-   projectId: "pruesimul",
-   storageBucket: "[pruesimul.appspot.com](http://pruesimul.appspot.com/)",
-   messagingSenderId: "737332937319",
-   appId: "1:737332937319:web:38b26f69881e3b071a0ac2",
-   measurementId: "G-GCJ486VNNN"
+ 
    };
    
    // Initialize Firebase
